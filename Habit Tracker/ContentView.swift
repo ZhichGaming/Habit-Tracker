ff25988560da9c2a8464d194ac4c2e49efd30670
ZhichGaming <nick.zhicheng@gmail.com>
2022-09-11 16:17:07 -0400

    Initial Commit

---
+++ Habit Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habit Tracker
+//
+//  Created by Nick on 2022-09-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Nick on 2022-09-11.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
